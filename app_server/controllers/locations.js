2f6e8e6bbcb4ea59f48b86c0cf13170b785e490c
saavadas <slava_0629@bk.ru>
2023-11-16 00:46:55 +0500

    Routes and controllers updated

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,16 @@
+/* GET 'home' page. */
+
+module.exports.homeList = function (req, res, next) {
+  res.render("index", { title: "Home" });
+};
+
+/* GET 'location info' page. */
+
+module.exports.locationInfo = function (req, res, next) {
+  res.render("index", { title: "Location info" });
+};
+/* GET 'add review' page. */
+
+module.exports.addReview = function (req, res, next) {
+  res.render("index", { title: "Add review" });
+};
