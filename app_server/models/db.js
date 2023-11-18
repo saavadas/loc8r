797fc97b87a8b74c2d20d7fda191d919bef5efb3
saavadas <slava_0629@bk.ru>
2023-11-18 20:37:40 +0500

    Added mongoose 6.10

---
+++ app_server/models/db.js
@@ -0,0 +1,40 @@
+var mongoose = require("mongoose");
+var dbURI = "mongodb://127.0.0.1:27017/Loc8r";
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(dbURI);
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose connected to " + dbURI);
+});
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose connection error: " + err);
+});
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose disconnected");
+});
+
+var gracefullShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log("Mongoose disconnected through " + msg);
+    callback();
+  });
+};
+
+process.once("SIGUSR2", function () {
+  gracefullShutdown("nodemon restart", function () {
+    process.kill(process.pid, "SIGUSR2");
+  });
+});
+
+process.once("SIGINT", function () {
+  gracefullShutdown("app termination", function () {
+    process.exit(0);
+  });
+});
+
+process.on("SIGTERM", function () {
+  gracefulShutdown("Railway app shutdown", function () {
+    process.exit(0);
+  });
+});
