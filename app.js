342a3680d5409aede96b307238b56043341b8b5c
saavadas <slava_0629@bk.ru>
2024-01-05 17:18:02 +0500

    Think i'm done with SPA 377

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("./app_api/models/db");
 
-var routes = require("./app_server/routes/index");
+// var routes = require("./app_server/routes/index");
 var routesApi = require("./app_api/routes/index");
 var users = require("./app_server/routes/users");
 
 var app = express();
 
 // view engine setup
+
 app.set("views", path.join(__dirname, "app_server", "views"));
 app.set("view engine", "jade");
 
@@ -22,10 +23,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "app_client")));
 
-app.use("/", routes);
+// app.use("/", routes);
 app.use("/users", users);
 app.use("/api", routesApi);
-
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "app_client", "index.html"));
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
